bd7aa76c21a0f0b5110538c7025dfcb25ee7da11
Connor Zanin <cnnrznn@gmail.com>
2019-11-17 21:15:11 -0500

    restructure the client

--- client/client.go
+++ client/client.go
@@ -2,14 +2,22 @@ package client
 
 import (
 	"context"
+	"io"
 
+	"github.com/cnnrznn/docs/document"
 	pb "github.com/cnnrznn/docs/editor"
 	"google.golang.org/grpc"
 )
 
 type Client struct {
-	Stub   pb.EditorClient
-	Stream pb.Editor_UpdateClient
+	stub   pb.EditorClient
+	stream pb.Editor_UpdateClient
+
+	inflight *document.Op
+	queue    []document.Op
+
+	in, out    chan document.Op // interface to user
+	fromServer chan document.Op // recv op from server
 }
 
 func New() (c *Client, err error) {
@@ -27,12 +35,76 @@ func New() (c *Client, err error) {
 		return
 	}
 
-	c.Stub = client
-	c.Stream, err = c.Stub.Update(context.Background())
+	c.stub = client
+	c.stream, err = c.stub.Update(context.Background())
+	if err != nil {
+		return
+	}
+
+	c.in, c.out = make(chan document.Op), make(chan document.Op)
+	c.fromServer = make(chan document.Op)
+
+	go func() {
+		for {
+			op, err := c.stream.Recv()
+			if err == io.EOF {
+				return
+			}
+			if err != nil {
+				return
+			}
+			c.fromServer <- document.Op{Version: int(op.Version),
+				Type: int(op.Type),
+				Char: op.Char[0],
+				Pos:  int(op.Pos)}
+		}
+	}()
 
 	return
 }
 
 func (c *Client) Close() {
-	c.Stream.CloseSend()
+	c.stream.CloseSend()
+}
+
+func (c *Client) push() {
+	if c.inflight != nil || len(c.queue) == 0 {
+		return
+	}
+
+	c.inflight = &c.queue[0]
+	c.queue = c.queue[1:]
+
+	op := *c.inflight
+
+	c.stream.Send(&pb.Op{Version: int64(op.Version),
+		Pos:  int64(op.Pos),
+		Type: int32(op.Type),
+		Char: []byte{op.Char}})
+}
+
+func (c *Client) Run() {
+	for {
+		select {
+		case op := <-c.in:
+			c.queue = append(c.queue, op)
+			c.push()
+		case op := <-c.fromServer:
+			if c.inflight != nil && c.inflight.Sender == op.Sender {
+				c.inflight = nil
+				c.push()
+			} else {
+				// transform op against c.queue
+				// transform c.queue against op
+				for i, qop := range c.queue {
+					copyOp := op
+					copyOp.Transform(qop)
+					c.queue[i].Transform(op)
+					op = copyOp
+				}
+
+				c.out <- op
+			}
+		}
+	}
 }
